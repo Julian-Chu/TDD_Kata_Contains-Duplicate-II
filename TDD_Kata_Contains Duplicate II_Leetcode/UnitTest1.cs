b5a58c77169db98d2b53296d95ee43c08160effb
USER <USER@MSI>
2017-02-16 10:49:51 +0100

    Add project files.

---
+++ TDD_Kata_Contains Duplicate II_Leetcode/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDD_Kata_Contains_Duplicate_II_Leetcode
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
