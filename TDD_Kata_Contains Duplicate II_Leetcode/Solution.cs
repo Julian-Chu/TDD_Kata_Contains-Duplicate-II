612e05a7a96797a27623f5c6b49e4e3d9a5fd354
Julian-Chu <yulang.chu@gmail.com>
2017-02-16 11:50:17 +0100

    add a failed test case: input k=0, should return false - Generate production code

---
+++ TDD_Kata_Contains Duplicate II_Leetcode/Solution.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TDD_Kata_Contains_Duplicate_II_Leetcode
+{
+    internal class Solution
+    {
+        internal object ContainsNearByDuplicate(int[] nums, int k)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- TDD_Kata_Contains Duplicate II_Leetcode/UnitTest1.cs
+++ TDD_Kata_Contains Duplicate II_Leetcode/UnitTest1.cs
@@ -6,9 +6,27 @@ namespace TDD_Kata_Contains_Duplicate_II_Leetcode
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        Solution solution;
+        [TestInitialize]
+        public void Initialize()
         {
+            solution = new Solution();
+        }
+
+        //Business logic: k must bigger than 0
+        [TestMethod]
+        public void ContainsNearbyDuplicate_Given_kIsZero_Return_False()
+        {
+            //Assign
+            int[] nums = { 1, 2, 3, 4, 1 };
+            int k = 0;
+
+            //Act
+            var expected = false;
+            var actual = solution.ContainsNearByDuplicate(nums, k);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
         }
     }
 }
